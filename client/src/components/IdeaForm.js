f335ece915fd08ac927282bacd130f2ceeec1c18
José Gabriel González <jgg032899@gmail.com>
2019-05-31 01:54:27 -0400

    Can create and update ideas, controller actions, React functions.

--- client/src/components/IdeaForm.js
+++ client/src/components/IdeaForm.js
@@ -4,17 +4,43 @@ import axios from 'axios'
 class IdeaForm extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            title: this.props.idea.title,
+            body: this.props.idea.body
+        }
+    }
+
+    handleInput = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    handleBlur = () => {
+        const idea = {
+            title: this.state.title,
+            body: this.state.body
+        }
+
+        axios.put(
+            '/api/v1/ideas/${this.props.idea.id}',
+            {
+                idea: idea
+            })
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => console.log(error))
     }
 
     render() {
         return (
             <div className="tile">
-                <form>
+                <form onBlur={this.handleBlur}>
                     <input className='input' type="text"
-                           name="title" placeholder='Enter a Title'/>
+                           name="title" placeholder='Enter a Title'
+                           value={this.state.title} onChange={this.handleInput}/>
                     <textarea className='input' name="body"
-                              placeholder='Describe your idea'></textarea>
+                              placeholder='Describe your idea'
+                              value={this.state.body} onChange={this.handleInput}></textarea>
                 </form>
             </div>
         );
